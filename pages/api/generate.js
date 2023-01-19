acb81627bde97acf4b5ef7889e0cda3fd3c21bec
Victoria Tran <victoriatran@Victorias-MacBook-Air.local>
2023-01-19 20:15:01 +0100

    setting up API, landing page

---
+++ pages/api/generate.js
@@ -0,0 +1,38 @@
+const bufferToBase64 = (buffer) => {
+  let arr = new Uint8Array(buffer);
+  const base64 = btoa(
+    arr.reduce((data, byte) => data + String.fromCharCode(byte), "")
+  );
+  return `data:image/png;base64,${base64}`;
+};
+const generateAction = async (req, res) => {
+  console.log("Recieved request");
+  const input = JSON.parse(req.body).input;
+
+  const response = await fetch(
+    `https://api-inference.huggingface.co/models/intotransit/victran`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.HF_AUTH_KEY}`,
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ inputs: input }),
+    }
+  );
+  // Check for different statuses to send the correct payload
+  if (response.ok) {
+    const buffer = await response.arrayBuffer();
+    // convert to base64
+    const base64 = bufferToBase64(buffer);
+    res.status(200).json({ image: base64 });
+  } else if (response.status == 503) {
+    const json = await response.json();
+    res.status(503).json(json);
+  } else {
+    const json = await response.json();
+    res.status(response.status).json({ error: response.statusText });
+  }
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,138 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
 
 const Home = () => {
+  // Don't retry more than 20 times
+  const maxRetries = 20;
+  const [input, setInput] = useState("");
+  const [img, setImg] = useState("");
+  //number of retries
+  const [retry, setRetry] = useState(0);
+  //number of retries left
+  const [retryCount, setRetryCount] = useState(maxRetries);
+  // Add isGenerating state
+  const [isGenerating, setIsGenerating] = useState(false);
+  // get prompt for img to display underneath
+  const [finalPrompt, setFinalPrompt] = useState("");
+  const onChange = (event) => {
+    setInput(event.target.value);
+  };
+  //add generateAction
+  const generateAction = async () => {
+    console.log("Generating...");
+
+    // Add this check to make sure there's no double click
+    if (isGenerating && retry == 0) return;
+
+    setIsGenerating(true);
+
+    if (retry > 0) {
+      setRetryCount((prevState) => {
+        if (prevState === 0) {
+          return 0;
+        } else {
+          return prevState - 1;
+        }
+      });
+      setRetry(0);
+    }
+
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "image/jpeg",
+      },
+      body: JSON.stringify({ input }),
+    });
+    const data = await response.json();
+
+    if (response.status == 503) {
+      console.log("Model is loading still :(");
+      setRetry(data.estimated_time);
+      return;
+    }
+    if (!response.ok) {
+      console.log(`Error: ${data.error}`);
+      // Stop Loading
+      setIsGenerating(false);
+      return;
+    }
+    // Set final prompt here
+    setFinalPrompt(input);
+    // Remove content from input box
+    setInput("");
+    setImg(data.image);
+    setIsGenerating(false);
+  };
+  const sleep = (ms) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  };
+  useEffect(() => {
+    const runRetry = async () => {
+      if (retryCount === 0) {
+        console.log(
+          `Model still loading after ${maxRetries} retrues. Try request again in 5 minutes.`
+        );
+        setRetryCount(maxRetries);
+        return;
+      }
+      console.log(`Trying again in ${retry} seconds.`);
+
+      await sleep(retry * 1000);
+      await generateAction();
+    };
+    if (retry === 0) {
+      return;
+    }
+    runRetry();
+  }, [retry]);
   return (
     <div className="root">
       <Head>
-        <title>AI Avatar Generator | buildspace</title>
+        <title>AI Avatar Generator | made with buildspace</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>your generator one-liner</h1>
+            <h1>Victoria AI Picture Generator</h1>
           </div>
           <div className="header-subtitle">
-            <h2>description of your generator</h2>
+            <h2>
+              Use AI to imagine me into anything! Make sure to refer to me as
+              "victran" in the prompt
+            </h2>
+          </div>
+          <div className="prompt-container">
+            <input className="prompt-box" value={input} onChange={onChange} />
+            <div className="prompt-buttons">
+              <a
+                className={
+                  isGenerating ? "generate-button loading" : "generate-button"
+                }
+                onClick={generateAction}
+              >
+                <div className="generate">
+                  {isGenerating ? (
+                    <span className="loader"></span>
+                  ) : (
+                    <p>Generate</p>
+                  )}
+                </div>
+              </a>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-avatar"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        {/* add ouput container if there's something in the img propoerty display this img */}
+        {img && (
+          <div className="output-content">
+            <Image src={img} width={512} height={512} alt={input}></Image>
+            {/* Add prompt here */}
+            <p>{finalPrompt}</p>
           </div>
-        </a>
+        )}
       </div>
     </div>
   );
